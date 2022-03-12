73213f9d97b9eba63b95271decffae5ea4a07682
ahmed-razic <ahmed.razic@gmail.com>
2022-03-12 15:22:43 +0100

    footer

--- src/components/SocialLinks/SocialLinks.jsx
+++ src/components/SocialLinks/SocialLinks.jsx
@@ -1,7 +1,26 @@
 import styles from './SocialLinks.module.css';
+import FaceBookIcon from './FaceBookIcon';
+import TwitterIcon from './TwitterIcon';
+import YoutubeIcon from './YoutubeIcon';
+import InstagramIcon from './InstagramIcon';
 
 function SocialLinks() {
-    return <div>SocialLinks</div>;
+    return (
+        <div className={styles.socialLinks}>
+            <a href="https://" className={styles.iconLink}>
+                <FaceBookIcon />
+            </a>
+            <a href="https://" className={styles.iconLink}>
+                <InstagramIcon />
+            </a>
+            <a href="https://" className={styles.iconLink}>
+                <TwitterIcon />
+            </a>
+            <a href="https://" className={styles.iconLink}>
+                <YoutubeIcon />
+            </a>
+        </div>
+    );
 }
 
 export default SocialLinks;
